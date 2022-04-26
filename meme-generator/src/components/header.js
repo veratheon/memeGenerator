d6314a9798110eeb5ea6222a035c8eed3b6a955f
Вера Максимовна <veramaksimovna@Veras-MacBook-Pro.local>
2022-04-26 22:35:05 +0300

    header+form is ready

---
+++ meme-generator/src/components/header.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <div className="header">
+            <div className="header-name">
+                <img src="Troll Face.svg" alt="Troll Face" className="troll-face"></img>
+                <h1>Meme Generator</h1>
+            </div>
+            <p className="project-number">React Course - Project 3</p>
+        </div>
+    )
+}
